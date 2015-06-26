ff7f81ef6e16cc39fdf3cc521857fde5926dfe99
Lakshman Sha <lakshmansha1992@gmail.com>
2015-06-26 10:11:05 +0000

    Changes made by Lakshman Sha (lakshmansha1992@gmail.com).

---
+++ Demo Hybrid/alert.js
@@ -0,0 +1,48 @@
+function(){
+    
+     // Global error handling
+    var showAlert = function(message, callback) {
+        navigator.notification.alert(message, callback || function () {
+        },"Demo App", 'OK');
+    };
+
+    var showError = function(message) {
+        showAlert(message, 'Error occured');
+    };
+    
+    var AppHelper = {
+
+        // Return user profile picture url
+        resolveProfilePictureUrl: function (id) {
+            if (id && id !== emptyGuid) {
+                return el.Files.getDownloadUrl(id);
+            } else {
+                return 'styles/images/avatar.png';
+            }
+        },
+
+        // Return current activity picture url
+        resolvePictureUrl: function (id) {
+            if (id && id !== emptyGuid) {
+                return el.Files.getDownloadUrl(id);
+            } else {
+                return '';
+            }
+        },
+
+        // Date formatter. Return date in d.m.yyyy format
+        formatDate: function (dateString) {
+            return kendo.toString(new Date(dateString), 'MMM d, yyyy');
+        },
+
+        // Current user logout
+        logout: function () {
+            return el.Users.logout();
+        },
+
+        autoSizeTextarea: function () {
+            var rows = $(this).val().split('\n');
+            $(this).prop('rows', rows.length + 1);
+        }
+    };
+}
